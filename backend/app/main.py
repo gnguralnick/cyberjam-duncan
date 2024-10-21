e702159e4f2dd80077716edbd4a6ea9dc3c64294
Gabe Guralnick <gnguralnick@gmail.com>
2024-10-21 17:06:27 -0500

    serve frontend from backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 
+from fastapi.staticfiles import StaticFiles
+
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
 load_dotenv(override=True)
 
-app = FastAPI()
+app = FastAPI(title="root app")
 
-app.add_middleware(
+api_app = FastAPI(title="api app")
+
+api_app.add_middleware(
     CORSMiddleware,
     allow_origins=os.environ.get("ALLOWED_ORIGINS"),
     allow_credentials=True,
@@ -15,6 +21,12 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@api_app.get('/')
+def root():
+    return {'message': 'Hello World'}
+
+app.mount("/api", api_app)
+
+# Serve Frontend
+
+app.mount("/", StaticFiles(directory="static", html=True), name="frontend")
