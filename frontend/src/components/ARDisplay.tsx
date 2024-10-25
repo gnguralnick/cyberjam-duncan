6e9c06e1a5fc0d41fc8f38d9ac5a95d0adb03a1e
Gabe Guralnick <gnguralnick@gmail.com>
2024-10-25 11:16:14 -0500

    idek

--- frontend/src/components/ARDisplay.tsx
+++ frontend/src/components/ARDisplay.tsx
@@ -1,20 +1,21 @@
 import { ARDisplayProps } from '../types';
-import { ARCanvas, ARMarker } from '@artcom/react-three-arjs'; // ts-ignore
-import { useGLTF } from '@react-three/drei';
+//import { ARCanvas, ARMarker } from '@artcom/react-three-arjs'; // ts-ignore
+// import { useGLTF } from '@react-three/drei';
 
-const Model = ({url}: {url: string}) => {
-    const { scene } = useGLTF(url);
+// const Model = ({url}: {url: string}) => {
+//     const { scene } = useGLTF(url);
 
-    return <primitive object={scene} />;
-}
+//     return <primitive object={scene} />;
+// }
 
 export const ARDisplay: React.FC<ARDisplayProps> = ({ qrCode, modelUrl, markerUrl }) => {
 
     console.log(qrCode, markerUrl, modelUrl);
 
-    return <ARCanvas sourceType="webcam" debugUIEnabled={false} detectionMode="mono_and_matrix" matrixCodeType="3x3">
-        <ARMarker type="pattern" patternUrl={markerUrl} debug={true} onMarkerFound={() => console.log('Marker found')} onMarkerLost={() => console.log('Marker lost')}>
-            <Model url={modelUrl} />
-        </ARMarker>
-    </ARCanvas>
+    // return <ARCanvas sourceType="webcam" debugUIEnabled={false} detectionMode="mono_and_matrix" matrixCodeType="3x3">
+    //     <ARMarker type="pattern" patternUrl={markerUrl} debug={true} onMarkerFound={() => console.log('Marker found')} onMarkerLost={() => console.log('Marker lost')}>
+    //         <Model url={modelUrl} />
+    //     </ARMarker>
+    // </ARCanvas>
+    return null;
 };
